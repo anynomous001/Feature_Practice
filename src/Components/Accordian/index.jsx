eb5873d3fc5f4605714e55730092f00d8603069a
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2023-12-20 20:50:39 +0530

    Multi selection Applied

--- src/Components/Accordian/index.jsx
+++ src/Components/Accordian/index.jsx
@@ -1,17 +1,25 @@
 import { data } from './data'
 import React from 'react'
+import './style.css'
 
 export default function Accordian() {
 
     const [selected, setSelected] = React.useState(null)
+    const [multiSelection, setMultiSelection] = React.useState(false)
 
     function handleClick(id) {
         selected === id ? setSelected(null) : setSelected(id)
+    }
+    function handleMultiClick() {
+        setMultiSelection(!multiSelection)
     }
     console.log(selected)
     return (
         <div>
             <div className="container">
+                {/* For Multi selection*/}
+
+                <button onClick={handleMultiClick}>Multi Selection</button>
                 {/* For single selection*/}
                 {
                     data && data.length > 0 ? (
@@ -20,7 +28,7 @@ export default function Accordian() {
                                 <h2 >{item.name}</h2>
                                 <span>+</span>
                                 {
-                                    selected == item.age ? <div>
+                                    selected == item.age || multiSelection ? <div>
                                         <p>{item.about}</p>
                                     </div> : null
                                 }
@@ -30,6 +38,7 @@ export default function Accordian() {
                         <h1>Data is not fetched</h1>
                     )
                 }
+
             </div>
 
 
